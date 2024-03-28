3faad5d976ae0211a774524ef33593064975c118
Bryson Coons <coonsbrysona@gmail.com>
2024-03-27 20:51:19 -0400

    20240327

---
+++ reagentFormula.js
@@ -0,0 +1,8 @@
+function isValid(formula){
+    if (formula.includes(1) && formula.includes(2)) return false;
+    if (formula.includes(3) && formula.includes(4)) return false;
+    if (formula.includes(5) && !formula.includes(6) || !formula.includes(5) && formula.includes(6)) return false;
+    if (!formula.includes(7) && !formula.includes(8)) return false;
+    
+    return true;
+  }
