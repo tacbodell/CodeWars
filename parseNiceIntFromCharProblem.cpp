9907b391d78baf9f6ca0c2d28d127c2bb4d28eb2
Bryson Coons <coonsbrysona@gmail.com>
2024-05-30 18:24:23 -0400

    20240530

--- parseNiceIntFromCharProblem.cpp
+++ parseNiceIntFromCharProblem.cpp
@@ -2,7 +2,7 @@
 //
 //Write a program that returns the girl's age (0-9) as an integer.
 //
-//Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+//Assume the test input string is always a valid  string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
 #include <string>
 
