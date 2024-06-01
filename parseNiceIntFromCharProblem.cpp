b0fe94c97bce54b8da654c86370334bb51b763f1
Bryson Coons <coonsbrysona@gmail.com>
2024-06-01 13:38:06 -0400

    20240601

--- parseNiceIntFromCharProblem.cpp
+++ parseNiceIntFromCharProblem.cpp
@@ -1,4 +1,4 @@
-//You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+//You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9
 //
 //Write a program that returns the gir's age (0-9) as an integer.
 //
